bbdafb28d0ac6307a4f6e6c89ffc83dc2706ebad
reut <reut1990@gmail.com>
2019-01-17 16:43:18 +0200

    Updates

---
+++ precache-manifest.b113d671ca1ca2b9abf10366b1d4bb7d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "82e95f9eda39f291f0753a37c5d770d5",
+    "url": "/bitcoin-project/static/media/coins.82e95f9e.png"
+  },
+  {
+    "revision": "a4e5edadde3983e46195",
+    "url": "/bitcoin-project/static/css/main.0a459b8c.chunk.css"
+  },
+  {
+    "revision": "79981285d4ac419f7329",
+    "url": "/bitcoin-project/static/js/1.79981285.chunk.js"
+  },
+  {
+    "revision": "2660abe61aaa91507a11",
+    "url": "/bitcoin-project/static/js/runtime~main.2660abe6.js"
+  },
+  {
+    "revision": "5b522f14ce5ea6a46b73a66b6c065be6",
+    "url": "/bitcoin-project/static/media/home.5b522f14.png"
+  },
+  {
+    "revision": "7375014d9195d674362ed60e6964efff",
+    "url": "/bitcoin-project/static/media/static.7375014d.png"
+  },
+  {
+    "revision": "a4e5edadde3983e46195",
+    "url": "/bitcoin-project/static/js/main.a4e5edad.chunk.js"
+  },
+  {
+    "revision": "f247296b48b1f365914f12cb64ba50ee",
+    "url": "/bitcoin-project/static/media/BTC.f247296b.png"
+  },
+  {
+    "revision": "83c6de25ed91d13a4f09fb5f11ca8853",
+    "url": "/bitcoin-project/static/media/delete.83c6de25.png"
+  },
+  {
+    "revision": "35411dfc21e4e5fc1574d981765b11d5",
+    "url": "/bitcoin-project/static/media/back.35411dfc.png"
+  },
+  {
+    "revision": "98ebeae5debb78cc9791fcd242258f2c",
+    "url": "/bitcoin-project/static/media/bitcoin.98ebeae5.jpg"
+  },
+  {
+    "revision": "dbc1ebf2892e0c2e5f31d677133085f5",
+    "url": "/bitcoin-project/index.html"
+  }
+];
